4e3421bef73606949ad1ba31180cb0acef5dbe60
Abdul <moizhus123@gmail.com>
2020-02-16 01:35:03 +0500

    --updated

---
+++ precache-manifest.16de0b80d499e36c8bc2218a32b95a2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9f994cdc3a12e099feba049206dbe41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72926737e4c562e5747c",
+    "url": "/static/css/main.fd812c9d.chunk.css"
+  },
+  {
+    "revision": "6df6649476787a40a898",
+    "url": "/static/js/2.4cf47bf0.chunk.js"
+  },
+  {
+    "revision": "f96c07d9f455e7cc5e03d5121c1c7a4e",
+    "url": "/static/js/2.4cf47bf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72926737e4c562e5747c",
+    "url": "/static/js/main.0f088028.chunk.js"
+  },
+  {
+    "revision": "4349572bcd43bbb0a38d",
+    "url": "/static/js/runtime-main.57fc9c35.js"
+  }
+]);
