04ce3e994a3cbd766dcfc0c1381914b5e532c9f2
Abdul <moizhus123@gmail.com>
2020-03-07 01:00:57 +0500

    fixed input size issue for mobile device

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ef8ed6eb56d3c32a7b4fdbe81e19a98a.js"
+  "/precache-manifest.8c6c02ccfeab9fa0407fd735a6a152df.js"
 );
 
 self.addEventListener('message', (event) => {
