a298bba78edbad132e678affa69319cf93f95e58
Abdul Moiz <moizhus123@gmail.com>
2021-02-26 11:30:30 +0500

    rgb hex

---
+++ precache-manifest.1c2705a8dc913b0030fb27be7e401f6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "533a8c7c71054fa99a87faf9be8b5d3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90d398c67895d6494e55",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "88358e1955939d0877fb",
+    "url": "/static/css/main.ebf468bc.chunk.css"
+  },
+  {
+    "revision": "90d398c67895d6494e55",
+    "url": "/static/js/2.275bc492.chunk.js"
+  },
+  {
+    "revision": "039ae57fb9e0813dbd5fe6e50474afd5",
+    "url": "/static/js/2.275bc492.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88358e1955939d0877fb",
+    "url": "/static/js/main.b567cacf.chunk.js"
+  },
+  {
+    "revision": "3e958b7e172146139f67",
+    "url": "/static/js/runtime-main.b1abbbac.js"
+  }
+]);
